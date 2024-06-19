2a39c81726f0ff7c3a5432ab810977e0eadef341
El profe <al.sepulvedam@profesor.duoc.cl>
2024-06-19 11:11:55 -0400

    Agregado principal

---
+++ principal.py
@@ -0,0 +1,21 @@
+from os import system
+system ('cls')
+
+
+
+while True:
+    print("Welcome to the calculator program!")
+    print("1. Sumar")
+    print("2. Restar")
+    print("0. Salir")
+    op=input("Ingrese una opcion")
+    match op:
+        case "1":
+            #sumar()
+            pass
+        case "2":
+            #restar()
+            pass
+        
+        case "0":
+            break
